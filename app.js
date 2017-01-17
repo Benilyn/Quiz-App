a523c0f0bf62cb9a2bc0ecf3f88e6d8fb8b9425f
Benilyn <benilyn.gonzales@gmail.com>
2017-01-17 09:55:08 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+	$('.start').click(function(){
+		$('.intro').addClass('hide');
+		alert('it is working for now');
+	});
+});
+
+/*
+	$('form:first').submit(function() {
+		$(this).addClass('hide');
+		alert('this is working');
+	});
+  */      
+    
+
+/*
+// ALERT WHEN NEXT BUTTON IS CLICKED
+alert('Nice try but that is not the correct answer.');
+alert('Good job! You got the correct answer.');
+
+
+// Selects next sibling
+$(event.target).next('form');
+
+// Counter for correct answer
+var correctAnswer = 0;
+*/
